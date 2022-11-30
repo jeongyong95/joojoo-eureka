6228aa389f4a3eabb41976d971e3709712d886ef
jeongyong <jeongyong95@gmail.com>
2022-11-30 17:55:23 +0900

    [EUREKA-001] eureka init

---
+++ src/test/java/com/joojeongyong/joojoo/eureka/JoojooEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joojeongyong.joojoo.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoojooEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
